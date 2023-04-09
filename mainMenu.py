5b965982d7ec7ef3abd076c7bdf9f6b081b43780
eppatten <eppatten@ncsu.edu>
2023-04-09 10:09:58 -0400

    Completed functionality of entire GUI and added new image files

--- mainMenu.py
+++ mainMenu.py
@@ -1,27 +1,50 @@
+import tkinter as tk
+from recyclableDetection import bigMeatyClaws
+from recyclingPickup import smallMeatyClaws
 
-def menu():
-    import tkinter as tk
+points = 0
 
+def menu(additionalPoints=0):
+    global points
+
+    points += additionalPoints
+    
+    def detectRecycle():
+        root.destroy()
+        menu(bigMeatyClaws())
+
+    def playGame():
+        try :
+            root.destroy()
+            smallMeatyClaws()
+        except:
+            menu()
+        
     # create the main window
     root = tk.Tk()
     root.title("Buaxite")
-    root.geometry("1000x1000")
+    root.geometry("1116x612")
 
     # create the title label
-    title_label = tk.Label(root, text="Buaxite", font=("Arial", 24))
-    title_label.pack(pady=20)
+    bg = tk.PhotoImage(file="backGroundImage.png")
+    canvas = tk.Canvas(root, width=1116, height = 612)
+    canvas.pack()
 
-    # create the point balance label
-    point_balance_label = tk.Label(root, text="Points: 0", font=("Arial", 12))
-    point_balance_label.pack(padx=20, pady=10)
-
+    canvas.create_image(0,0, anchor="nw", image=bg)
+    
     # create the play button
-    play_button = tk.Button(root, text="Play", font=("Arial", 16), width=10)
-    play_button.pack(pady=10)
+    button = tk.Button(root, width=9, height=1, text="Play", font=("Arial", 16), bg='#f6bded', command=playGame)
+    canvas.create_window(603, 503, anchor="nw", window=button)
 
     # create the scan button
-    scan_button = tk.Button(root, text="Scan", font=("Arial", 16), width=10)
-    scan_button.pack(pady=10)
+    scan_button = tk.Button(root, width=9, height=1, text="Scan", font=("Arial", 16), bg='#f6bded', command=detectRecycle)
+    canvas.create_window(378, 503, anchor="nw", window=scan_button)
+
+    # create the scan button
+    point_label = tk.Label(root, width=9, height=1, text="Points: " + str(points), font=("Arial", 16), bg='#f6bded')
+    canvas.create_window(500, 87, anchor="nw", window=point_label)
 
     # start the main loop
     root.mainloop()
+
+menu()
